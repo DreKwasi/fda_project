8666f99c0b563b5f4727e6650c92151427ab273d
DreKwasi <andrewsboateng137@gmail.com>
2023-05-26 21:53:11 +0000

    Included data editor and ignored regex for str search

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,9 @@ import numpy as np
 from st_pages import Page, show_pages
 from helper_func import data_parser, styles, aiohttp_func
 import asyncio
+from streamlit_extras.no_default_selectbox import selectbox
 
-# from streamlit_extras.app_logo import add_logo
+
 # import cProfile
 # import pstats
 # import io
@@ -13,31 +14,19 @@ RELEVANT_COLS = [
     "product_id",
     "client_id",
     "product_category",
-    "product_sub_category",
-    "registration_number",
     "product_name",
-    "generic_name",
-    "strength",
     "active_ingredient",
-    "representative_company_local_agent_applicant",
     "manufacturer",
     "registration_type",
     "status",
     "country_origin",
-    "region",
     "applicant",
-    "registerd_importers",
-    "client_name",
-    "postal_address",
-    "sim_contact",
-    "telephone_number",
-    "email",
 ]
 
 
 st.set_page_config(
-    page_title="Market Registry",
-    page_icon="🏦",
+    page_title="Product Registry",
+    page_icon="🚨",
     initial_sidebar_state="expanded",
     layout="wide",
 )
@@ -48,24 +37,25 @@ styles.remote_css("https://fonts.googleapis.com/icon?family=Material+Icons")
 
 show_pages(
     [
-        Page("streamlit_app.py", "Home", "🏪"),
-        Page("st_pages/analyze.py", "Analytics", "💳"),
-        Page("st_pages/search.py", "Product Indexing", "📈"),
+        Page("streamlit_app.py", "Product Indexing", "🕵️"),
+        Page("st_pages/analyze.py", "Analytics", "📈"),
     ]
 )
 
 # asyncio.run(aiohttp_func.main_async_call())
 data = data_parser.read_data("registered_products.parquet")
 
-if st.sidebar.button("Update Data", type="primary"):
-    st.cache_data.clear()
-    with st.spinner("Accessing FDA Database"):
-        asyncio.run(aiohttp_func.main_async_call())
+if st.experimental_user in ["test@localhost.com", "andrewsboateng137@gmail.com"]:
+    if st.sidebar.button("Update Data", type="primary"):
+        st.cache_data.clear()
+        with st.spinner("Accessing FDA Database"):
+            asyncio.run(aiohttp_func.main_async_call())
 
 
 st.sidebar.header("Filter")
-
-st.sidebar.write("Select A Product Category")
+product_list = data["product_name"].unique().tolist()
+product_list.sort()
+st.sidebar.write("**Product Category**")
 
 product_category = st.sidebar.multiselect(
     "prod_cat", options=data["product_category"].unique(), label_visibility="collapsed"
@@ -73,21 +63,19 @@ product_category = st.sidebar.multiselect(
 if product_category:
     data = data[data["product_category"].isin(product_category)]
 
-st.sidebar.write("Select A Product Sub Category")
-product_sub_category = st.sidebar.multiselect(
-    "prod_sub_cat",
-    options=data["product_sub_category"].unique(),
-    label_visibility="collapsed",
+st.sidebar.write("**Applicant**")
+applicant_list = st.sidebar.multiselect(
+    "applicant", options=data["applicant"].unique(), label_visibility="collapsed"
 )
-if product_sub_category:
-    data = data[data["product_sub_category"].isin(product_sub_category)]
+if applicant_list:
+    data = data[data["applicant"].isin(applicant_list)]
 
-st.sidebar.write("Select a Manufacturer")
-manufacturer = st.sidebar.multiselect(
+st.sidebar.write("**Manufacturer**")
+manufacturer_list = st.sidebar.multiselect(
     "manufacturer", options=data["manufacturer"].unique(), label_visibility="collapsed"
 )
-if manufacturer:
-    data = data[data["manufacturer"].isin(manufacturer)]
+if manufacturer_list:
+    data = data[data["manufacturer"].isin(manufacturer_list)]
 
 
 st.title("Product Indexing")
@@ -96,12 +84,45 @@ st.write(
 )
 
 
-col1, col2 = st.columns([3, 1])
+col1, col2 = st.columns([5, 1])
 product_name = col1.text_input(
-    "Search", placeholder="Search Product Name ... ", label_visibility="collapsed"
+    "Search", placeholder="Search Product Name ...", label_visibility="collapsed"
 )
+
 button_clicked = col2.button("🔍", type="primary")
 
+st.subheader("Registered Products Table")
 
-data = data[data["product_name"].str.contains(product_name, case=False)]
-st.dataframe(data[RELEVANT_COLS])
+if product_name:
+    data = data[
+        data["product_name"].str.contains(product_name, case=False, regex=False)
+    ]
+
+    data["Show Product Details"] = False
+    RELEVANT_COLS.insert(0, "Show Product Details")
+    main_data_holder = st.empty()
+    checked_data_holder = st.empty()
+
+    show_data = data[RELEVANT_COLS]
+    checked_df = main_data_holder.experimental_data_editor(data[RELEVANT_COLS])
+
+    if "checked_df" not in st.session_state:
+        st.session_state["checked_df"] = True
+    checked_df = checked_df[
+        checked_df["Show Product Details"] == st.session_state["checked_df"]
+    ]
+
+    if checked_df.shape[0] == 1:
+        main_data_holder.empty()
+        unchecked_df = checked_data_holder.experimental_data_editor(checked_df)
+        # st.write(unchecked_df["Show Product Details"].values[0])
+        if unchecked_df["Show Product Details"].values[0] == False:
+            st.session_state["checked_df"] = False
+            st.experimental_rerun()
+    else:
+        checked_data_holder.empty()
+        st.session_state["checked_df"] = True
+
+
+else:
+    st.dataframe(data[RELEVANT_COLS])
