192b98a732432a0844a392c174b0a9e9fa2565c5
DreKwasi <andrewsboateng137@gmail.com>
2023-05-28 22:42:57 +0000

    basic charts for analysis

--- helper_func/charts.py
+++ helper_func/charts.py
@@ -1,3 +1,12 @@
 import plotly_express as px
 import pandas as pd
 import streamlit as st
+
+
+def trend_chart(data):
+    fig = px.bar(data, x="registration_date", y="number_of_registrations")
+    st.plotly_chart(fig, use_container_width=True)
+
+def grouped_chart(data):
+    fig = px.bar(data, x="product_category", y="number_of_registrations", color="product_category")
+    st.plotly_chart(fig, use_container_width=True)
