331ba27181580ea417a13f5dbae72324acd1074b
DreKwasi <andrewsboateng137@gmail.com>
2023-05-31 18:40:28 +0000

    Disclaimer and FDA Link

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,7 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
 from st_pages import Page, show_pages
 from helper_func import data_parser, styles, aiohttp_func, forms, utils
-import asyncio
-from streamlit_lottie import st_lottie_spinner, st_lottie # pip install streamlit-lottie
+from streamlit_lottie import st_lottie_spinner
 
 
 # import cProfile
@@ -37,18 +34,30 @@ show_pages(
     [
         Page("streamlit_app.py", "Product Indexing", "🕵️"),
         Page("st_pages/analyze.py", "Analytics", "📈"),
-        Page("st_pages/contact.py", "Contact Form", "📧"),
-        
+        Page("st_pages/contact.py", "Give Feedback", "✍️"),
+        Page("st_pages/newsletter.py", "Join the Newsletter", "✉"),
     ]
 )
+
+with st.spinner("Extracting Data from FDA Database ..."):
+    with st_lottie_spinner(
+        utils.load_lottiefile("assets/animations/wireless_data.json"),
+        height=500,
+    ):
+        aiohttp_func.connect_to_fda()
+        # st.sidebar.success("Registered Products Data Uploaded to Database")
+
 data = data_parser.read_data("registered_products.parquet")
 
 if st.experimental_user.email in ["test@localhost.com", "andrewsboateng137@gmail.com"]:
     if st.sidebar.button("Update Data from FDA", type="primary"):
         st.cache_data.clear()
-        with st.spinner("Accessing FDA Database ..."):
-            with st_lottie_spinner(utils.load_lottiefile("assets/animations/wireless_data.json"), height=500):
-                asyncio.run(aiohttp_func.main_async_call())
+        with st.spinner("Extracting Data from FDA Database ..."):
+            with st_lottie_spinner(
+                utils.load_lottiefile("assets/animations/wireless_data.json"),
+                height=500,
+            ):
+                aiohttp_func.connect_to_fda()
 
 
 query_params = st.experimental_get_query_params()
@@ -62,11 +71,19 @@ if query_params:
 
 
 def product_list_view(data):
+    st.sidebar.write("[Click Here to Visit the FDA Registered Products Database](http://196.61.32.245:98/publicsearch)")
     st.sidebar.header("Filter")
-    
+
     st.sidebar.write("**Select Number of Entries**")
-    num = st.sidebar.slider("number_of_entries", min_value=5, max_value=100, value=10, label_visibility="collapsed", step=10)
-    
+    num = st.sidebar.slider(
+        "number_of_entries",
+        min_value=5,
+        max_value=100,
+        value=10,
+        label_visibility="collapsed",
+        step=10,
+    )
+
     product_list = data["product_name"].unique().tolist()
     product_list.sort()
     st.sidebar.write("**Product Category**")
@@ -86,19 +103,36 @@ def product_list_view(data):
     if applicant_list:
         data = data[data["applicant"].isin(applicant_list)]
 
-    st.sidebar.write("**Manufacturer**")
-    manufacturer_list = st.sidebar.multiselect(
-        "manufacturer",
-        options=data["manufacturer"].unique(),
+    st.sidebar.write("**Country Origin**")
+    country_origin_list = st.sidebar.multiselect(
+        "country_origin",
+        options=data["country_origin"].unique(),
         label_visibility="collapsed",
     )
-    if manufacturer_list:
-        data = data[data["manufacturer"].isin(manufacturer_list)]
+    if country_origin_list:
+        data = data[data["country_origin"].isin(country_origin_list)]
 
     st.title("Product Indexing")
     st.write(
-        "This is a product indexing tool. Search through over 40,000 registered products"
+        "This is a Product Indexing/Analytics tool. Search through over 40,000 registered products in Ghana. "
     )
+    with st.expander("**DISCLAIMER**"):
+        st.write(
+            """
+The data provided by this tool for fetching registered products from the FDA website is intended for general informational purposes only. While efforts are made to ensure the accuracy and reliability of the information, it is important to note that the data is subject to change and may not always reflect the most current updates.
+
+The Tool updates its database regularly, typically on an hourly basis. However, due to the dynamic nature of the information and potential delays in data synchronization, there may be instances where the data retrieved by this tool is not up-to-date or may not include the latest changes.
+
+It is crucial to understand that this tool does not guarantee the accuracy, completeness, or timeliness of the information provided. Users should exercise their own judgment and verify any information obtained from this tool by cross-referencing it with official sources or contacting the FDA directly.
+
+Furthermore, this tool is not intended to substitute professional advice or guidance from qualified healthcare professionals or regulatory authorities. It is always advisable to consult appropriate sources and experts before making any decisions or taking actions based on the data obtained from this tool.
+
+By using this tool, you acknowledge and accept that the information provided is not exhaustive or definitive. The developers and providers of this tool disclaim any liability for any inaccuracies, errors, omissions, or damages arising from the use or reliance on the information provided by this tool.
+
+Please be aware that the FDA's official website should be considered the primary source of information for the most accurate and up-to-date data regarding registered products.
+
+By proceeding to use this tool, you agree to these terms and conditions and understand the limitations of the data provided."""
+        )
 
     col1, col2 = st.columns([5, 1])
     product_name = col1.text_input(
